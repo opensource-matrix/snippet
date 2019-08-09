a7e2589ce51edf3b11250c03b8b031d8866e00dc
Zack <mcodikid@gmail.com>
2019-08-08 18:34:40 -0600

    Update publish system.

---
+++ 1up.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var jsonObj = fs.readFileSync('./package.json').toString();
+jsonObj = JSON.parse(jsonObj);
+
+var a = jsonObj.version.split('.'),
+    major = a[0],
+    minor = a[1],
+    release = a[2];
+
+release = (parseInt(release) + 1).toString();
+
+var finVer = [
+  major,
+  minor,
+  release
+].join('.');
+
+jsonObj.version = finVer;
+
+fs.writeFileSync('./package.json', JSON.stringify(jsonObj));
--- publish.bat
+++ publish.bat
@@ -2,3 +2,5 @@
 git add *
 git commit -m "%*"
 git push
+node 1up.js
+npm publish
