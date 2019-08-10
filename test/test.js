f0ac2b5a99348a4ec237e89eaef0d5150a2006fb
Zack <mcodikid@gmail.com>
2019-08-09 18:07:40 -0600

    Add a method for testing snippets.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,16 @@
 /* Test the Snippet Library */
 var snippet = require('../package.js'),
-    snippet_log = require('../snippet_log.js');
+    snippet_log = require('../snippet_log.js'),
+    test_js = require('./test_js.js');
 
 try {
   console.log(snippet.checkLanguage('javascript'));
   console.log(snippet.checkName('hello_world_js'));
   console.log(snippet.getSnippetsByLanguage('javascript'));
   console.log(snippet.getSnippetByName('hello_world_js'));
-  console.log(snippet.getSnippets());
+  console.log(snippet.getSnippets(), snippet_log);
+
+  test_js(snippet.getSnippetsByLanguage());
 } catch(err) {
   snippet_log('error', 'Testing error : ' + err.message);
   process.exit(1);
---
+++ test/test_js.js
@@ -0,0 +1,9 @@
+module.exports = function(js, snippet_log) {
+  Object.keys(js).forEach(function(key) {
+    try {
+      eval(js[key]);
+    } catch(e) {
+      snippet_log('error', 'Testing error : ' + e);
+    }
+  })
+}
