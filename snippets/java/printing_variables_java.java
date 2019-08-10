6d8fbb8af831902f1c535eac26c21e327c36b11e
Zack <mcodikid@gmail.com>
2019-08-10 00:43:09 -0600

    Update package.

--- snippets/java/printing_variables_java.java
+++ snippets/java/printing_variables_java.java
@@ -1,8 +1,10 @@
+package snippets.java;
+
 public class printing_variables_java {
   public static void main(String[] args) {
     int myInteger = 0;
     System.out.println(myInteger);
-    
+
     String myString = "Hello, World!";
     System.out.println(myString);
   }
